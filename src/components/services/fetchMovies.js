aaad34f4ed80dbad489ea1547a4e106f280409df
Bohdan <yashbon@ukr.net>
2023-07-02 12:58:33 +0300

    Update fetchMovies.js

--- src/components/services/fetchMovies.js
+++ src/components/services/fetchMovies.js
@@ -29,14 +29,14 @@ const getImages = async (pictureName, page) => {
 // const axios = require('axios');
 
 const getMovies = async () => {
-    const options = {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmQ1ODQ5ZmUwZGY3ZTQwM2Y5ODFjMzE5MjYzMWUzNCIsInN1YiI6IjY0OWMxZmIxOTYzODY0MDBlM2JiOTJkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9sc78zQWEUmWayIyQricXEa9zOuE3w2J6KYL2093KGg'
-        }
-    };
+    // const options = {
+    //     method: 'GET',
+    //     url: 'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+    //     headers: {
+    //         accept: 'application/json',
+    //         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmQ1ODQ5ZmUwZGY3ZTQwM2Y5ODFjMzE5MjYzMWUzNCIsInN1YiI6IjY0OWMxZmIxOTYzODY0MDBlM2JiOTJkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9sc78zQWEUmWayIyQricXEa9zOuE3w2J6KYL2093KGg'
+    //     }
+    // };
     const URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=9bd5849fe0df7e403f981c3192631e34'
 
     const response = await axios.get(URL)
